c5f5e3576617035a46e0c16ea61b9dcc54b9b20e
ShrimpDev <muhammadfaheemwahid@gmail.com>
2024-10-18 02:33:27 +0800

    complete learning vue crash course

---
+++ src/components/JobListing.vue
@@ -0,0 +1,83 @@
+<script setup>
+import { defineProps, ref, computed } from "vue";
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  job: {
+    type: Object,
+    required: true,
+    default: () => ({
+      description: "No description available.",
+      type: "N/A",
+      title: "Untitled Job",
+      salary: "N/A",
+      location: "N/A",
+      id: 0,
+    }),
+  },
+});
+
+const showFullDescription = ref(false);
+
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+};
+
+const truncatedDescription = computed(() => {
+  let description = props.job.description || "No description available.";
+
+  if (!showFullDescription.value) {
+    // Ensure description is a string before calling substring
+    if (typeof description === "string") {
+      description =
+        description.length > 90
+          ? description.substring(0, 90) + "..."
+          : description;
+    } else {
+      description = "No description available.";
+    }
+  }
+
+  return description;
+});
+</script>
+
+<template>
+  <div class="bg-white rounded-xl shadow-md relative">
+    <div class="p-4">
+      <div class="mb-6">
+        <div class="text-gray-600 my-2">{{ job.type || "N/A" }}</div>
+        <h3 class="text-xl font-bold">{{ job.title || "Untitled Job" }}</h3>
+      </div>
+
+      <div class="mb-5">
+        <div>
+          {{ truncatedDescription }}
+        </div>
+        <button
+          @click="toggleFullDescription"
+          class="text-green-500 hover:text-green-600 mb-5"
+        >
+          {{ showFullDescription ? "Less" : "More" }}
+        </button>
+      </div>
+
+      <h3 class="text-green-500 mb-2">{{ job.salary || "N/A" }} / Year</h3>
+
+      <div class="border border-gray-100 mb-5"></div>
+
+      <div class="flex flex-col lg:flex-row justify-between mb-4">
+        <div class="text-orange-700 mb-3">
+          <i class="pi pi-map-marker text-orange-700"></i>
+          {{ job.location || "N/A" }}
+        </div>
+        <RouterLink
+          :to="'/jobs/' + job.id"
+          class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+        >
+          Read More
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
